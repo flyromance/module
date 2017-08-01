a1ab3ecd2dc09e5196553320d67c97f17ed65f7a
flyromance <814885004@qq.com>
2017-08-01 22:05:38 +0800

    fix

--- require/demo/public/js/lib/require.config.js
+++ require/demo/public/js/lib/require.config.js
@@ -2,7 +2,7 @@ require.config({
 	// 来源1：如果有data-main，会根据data-main配置一个baseUrl
 	// 来源2：这边配置了baseUrl，回去覆盖requirejs内部的baseUrl
 	// 作用：如果require或者define的name不是绝对路径，就要与baseUrl合成，生成一个路径作为script的src
-    baseUrl: '', 
+    baseUrl: '../public', 
 
     // 不配置baseUrl，就要写成 a: 'js/a'
     // require或者define的module_name再paths中时，用paths中的值，如果此值为相对路径就与baseUrl相结合，如果为绝对路径直接作为src
